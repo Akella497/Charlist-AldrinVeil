3a975e05eacc06c5f0e6fa7a5199535f9a28fdf3
Akella497 <kamoartem47@gmail.com>
2026-03-16 23:53:57 +0300

    Update GitHub OAuth scope to include 'repo'

--- api/auth/login.js
+++ api/auth/login.js
@@ -4,7 +4,7 @@
 export default function handler(req, res) {
   const clientId = process.env.GITHUB_CLIENT_ID;
   const redirectUri = `${process.env.SITE_URL}/api/auth/callback`;
-  const scope = 'read:user';
+  const scope = 'read:user repo';
 
   const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}`;
   res.redirect(url);
